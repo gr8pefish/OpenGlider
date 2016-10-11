2b748e1fa2cafeb7a492990d5e32c4100525947f
Max <xwason@gmail.com>
2016-10-11 11:59:34 -0700

    Update capability attach event to non-deprecated call

--- src/main/java/gr8pefish/openglider/common/event/ServerEventHandler.java
+++ src/main/java/gr8pefish/openglider/common/event/ServerEventHandler.java
@@ -3,6 +3,7 @@ package gr8pefish.openglider.common.event;
 import gr8pefish.openglider.common.capabilities.OpenGliderCapabilities;
 import gr8pefish.openglider.common.capabilities.PlayerGlidingCapability;
 import gr8pefish.openglider.common.lib.ModInfo;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
@@ -11,9 +12,9 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ServerEventHandler {
 
     @SubscribeEvent
-    public void onAttachCapability(AttachCapabilitiesEvent.Entity event) {
-        if (event.getEntity() instanceof EntityPlayer) {
-            if (!event.getEntity().hasCapability(OpenGliderCapabilities.GLIDING_CAPABILITY, null)) {
+    public void onAttachCapability(AttachCapabilitiesEvent<Entity> event) {
+        if (event.getObject() instanceof EntityPlayer) {
+            if (!event.getObject().hasCapability(OpenGliderCapabilities.GLIDING_CAPABILITY, null)) {
                 event.addCapability(new ResourceLocation(ModInfo.MODID + "." + ModInfo.GLIDING_CAPABILITY_STRING), new PlayerGlidingCapability());
             }
         }
