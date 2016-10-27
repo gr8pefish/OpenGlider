f1961158f08076a485e11c655d095ab8012ca1a3
Max <xwason@gmail.com>
2016-10-27 10:13:21 -0700

    More work on tracking

--- src/main/java/gr8pefish/openglider/common/event/ServerEventHandler.java
+++ src/main/java/gr8pefish/openglider/common/event/ServerEventHandler.java
@@ -4,16 +4,14 @@ import gr8pefish.openglider.common.capabilities.OpenGliderCapabilities;
 import gr8pefish.openglider.common.capabilities.PlayerGlidingCapability;
 import gr8pefish.openglider.common.helper.OpenGliderPlayerHelper;
 import gr8pefish.openglider.common.lib.ModInfo;
-import gr8pefish.openglider.common.network.PacketClientGliding;
 import gr8pefish.openglider.common.network.PacketHandler;
+import gr8pefish.openglider.common.network.PacketUpdateClientTarget;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class ServerEventHandler {
@@ -49,11 +47,16 @@ public class ServerEventHandler {
 
     @SubscribeEvent
     public void onTrack(net.minecraftforge.event.entity.player.PlayerEvent.StartTracking event) {
-        if (event.getTarget() instanceof EntityPlayer) {
-            EntityPlayer tracked = (EntityPlayer) event.getTarget();
-            if (OpenGliderCapabilities.getIsGliding(tracked)) {
-                PacketHandler.HANDLER.sendTo(new PacketClientGliding(PacketClientGliding.IS_GLIDING), (EntityPlayerMP) event.getEntityPlayer());
-            }
+        EntityPlayer tracker = event.getEntityPlayer(); //the tracker
+        Entity target = event.getTarget(); //the target that is being tracked
+        if (OpenGliderCapabilities.getIsGliding(tracker)){ //check that the tracker has capability (is it really checking if the tracker has the cap)
+            PacketHandler.HANDLER.sendTo(new PacketUpdateClientTarget(PacketUpdateClientTarget.IS_GLIDING), (EntityPlayerMP) tracker); //send a packet to their (ambiguous, who is their?) client to update their target
         }
     }
+
+//    <tterrag|ZZZzzz> You are sending the info of the player they are tracking, to the tracker
+//    <tterrag|ZZZzzz> So you want to check that the target has the capability
+//    <tterrag|ZZZzzz> er
+//    <tterrag|ZZZzzz> The tracker*
+//    <tterrag|ZZZzzz> and send a packet to their client to update the target
 }
