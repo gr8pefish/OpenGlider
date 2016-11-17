2df36cf4f2946994d775e597e08afb67e2a95598
gr8pefish <gr8pefish@gmail.com>
2016-11-17 11:12:41 -0700

    Start of rendering inWorld perspective projection (as opposed to inGui orthogonal)

--- src/main/java/gr8pefish/openglider/client/event/ClientEventHandler.java
+++ src/main/java/gr8pefish/openglider/client/event/ClientEventHandler.java
@@ -1,17 +1,17 @@
 package gr8pefish.openglider.client.event;
 
+import gr8pefish.openglider.client.model.ModelGlider;
 import gr8pefish.openglider.common.capabilities.OpenGliderCapabilities;
 import gr8pefish.openglider.common.helper.OpenGliderPlayerHelper;
+import gr8pefish.openglider.common.lib.ModInfo;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -24,37 +24,39 @@ public class ClientEventHandler extends Gui {
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onRender(RenderPlayerEvent.Pre event) {
-
         if (event.getEntity() instanceof EntityPlayer) {
             EntityPlayer playerEntity = (EntityPlayer) event.getEntity();
             if (OpenGliderCapabilities.getIsGliderDeployed((EntityPlayer) event.getEntity())) {
                 if (!OpenGliderPlayerHelper.shouldBeGliding(playerEntity)) return;
-
-                float partialTicks = Minecraft.getMinecraft().getRenderPartialTicks();
-                //            double interpolatedPitch = (playerEntity.prevRotationPitch + (playerEntity.rotationPitch - playerEntity.prevRotationPitch) * partialTicks);
-                double interpolatedYaw = (playerEntity.prevRotationYaw + (playerEntity.rotationYaw - playerEntity.prevRotationYaw) * partialTicks);
-
-                //ToDo Ask about entityPlayerMP and related stuffs
-
-                GlStateManager.pushMatrix();
-                //7. Set position back to normal
-                GlStateManager.translate(event.getX(), event.getY(), event.getZ());
-                //6. Redo yaw rotation
-                GlStateManager.rotate((float) -interpolatedYaw, 0, 1, 0);
-                //5. Move back to (0, 0, 0)
-                GlStateManager.translate(0, playerEntity.height / 2f, 0);
-                //4. Rotate about x, so player leans over forward (z direction is forwards)
-                GlStateManager.rotate(90, 1, 0, 0);
-                //3. So we rotate around the centre of the player instead of the bottom of the player
-                GlStateManager.translate(0, -playerEntity.height / 2f, 0);
-                //2. Undo yaw rotation (this will make the player face +z (south)
-                GlStateManager.rotate((float) interpolatedYaw, 0, 1, 0);
-                //1. Set position to (0, 0, 0)
-                GlStateManager.translate(-event.getX(), -event.getY(), -event.getZ());
+                rotateToHorizontal(event.getEntityPlayer(), event.getX(), event.getY(), event.getZ());
                 this.needToPop = true;
-                return;
             }
         }
+    }
+
+    private void rotateToHorizontal(EntityPlayer playerEntity, double x, double y, double z){
+        float partialTicks = Minecraft.getMinecraft().getRenderPartialTicks();
+        //            double interpolatedPitch = (playerEntity.prevRotationPitch + (playerEntity.rotationPitch - playerEntity.prevRotationPitch) * partialTicks);
+        double interpolatedYaw = (playerEntity.prevRotationYaw + (playerEntity.rotationYaw - playerEntity.prevRotationYaw) * partialTicks);
+
+        //ToDo Ask about entityPlayerMP and related stuffs
+
+        GlStateManager.pushMatrix();
+        //7. Set position back to normal
+        GlStateManager.translate(x, y, z);
+        //6. Redo yaw rotation
+        GlStateManager.rotate((float) -interpolatedYaw, 0, 1, 0);
+        //5. Move back to (0, 0, 0)
+        GlStateManager.translate(0, playerEntity.height / 2f, 0);
+        //4. Rotate about x, so player leans over forward (z direction is forwards)
+        GlStateManager.rotate(90, 1, 0, 0);
+        //3. So we rotate around the centre of the player instead of the bottom of the player
+        GlStateManager.translate(0, -playerEntity.height / 2f, 0);
+        //2. Undo yaw rotation (this will make the player face +z (south)
+        GlStateManager.rotate((float) interpolatedYaw, 0, 1, 0);
+        //1. Set position to (0, 0, 0)
+        GlStateManager.translate(-x, -y, -z);
+
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
@@ -72,7 +74,7 @@ public class ClientEventHandler extends Gui {
                 EntityPlayer playerEntity = Minecraft.getMinecraft().thePlayer;
                 if (OpenGliderCapabilities.getIsGliderDeployed(playerEntity)) {
                     if (OpenGliderPlayerHelper.shouldBeGliding(playerEntity)) {
-                        renderTriangle(event);
+//                        renderTriangle(event);
                     }
                 }
             }
@@ -92,5 +94,38 @@ public class ClientEventHandler extends Gui {
         GlStateManager.popMatrix();
 
     }
+
+    @SubscribeEvent
+    public void onRenderOverlay(RenderWorldLastEvent event){
+        if (Minecraft.getMinecraft().gameSettings.thirdPersonView == 0) {//first person perspective
+            EntityPlayer playerEntity = Minecraft.getMinecraft().thePlayer;
+            if (OpenGliderCapabilities.getIsGliderDeployed(playerEntity)) {
+                if (OpenGliderPlayerHelper.shouldBeGliding(playerEntity)) {
+                    renderTriangle(event);
+                }
+            }
+        }
+    }
+
+    private final ModelGlider modelGlider = new ModelGlider();
+
+    private void renderTriangle(RenderWorldLastEvent event){
+
+        EntityPlayer entityPlayer = Minecraft.getMinecraft().thePlayer;
+
+        GlStateManager.pushMatrix(); //push matrix
+        Minecraft.getMinecraft().getTextureManager().bindTexture(ModInfo.MODEL_GLIDER_TEXTURE_RL); //bind texture
+        modelGlider.setRotationAngles(entityPlayer.limbSwing, entityPlayer.limbSwingAmount, entityPlayer.getAge(), entityPlayer.rotationYawHead, entityPlayer.rotationPitch, 1, entityPlayer); //update rotation (Not working)
+        modelGlider.render(entityPlayer, entityPlayer.limbSwing, entityPlayer.limbSwingAmount, entityPlayer.getAge(), entityPlayer.rotationYawHead, entityPlayer.rotationPitch, 1); //render
+        GlStateManager.popMatrix(); //pop matrix
+
+        //doesn't rotate correctly
+        rotateToHorizontal(entityPlayer, entityPlayer.posX, entityPlayer.posY, entityPlayer.posZ);
+        GlStateManager.popMatrix();
+
+
+    }
+
+
 
 }
