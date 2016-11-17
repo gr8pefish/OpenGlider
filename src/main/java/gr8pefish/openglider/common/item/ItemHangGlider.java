0e3b3b081857c5954a8aba50f984a6b56c9d1c77
gr8pefish <gr8pefish@gmail.com>
2016-11-16 21:08:11 -0700

    Add icon and code to display glider texture differently when deployed vs not deployed

--- src/main/java/gr8pefish/openglider/common/item/ItemHangGlider.java
+++ src/main/java/gr8pefish/openglider/common/item/ItemHangGlider.java
@@ -5,17 +5,23 @@ import gr8pefish.openglider.common.capabilities.OpenGliderCapabilities;
 import gr8pefish.openglider.common.lib.ModInfo;
 import gr8pefish.openglider.common.network.PacketHandler;
 import gr8pefish.openglider.common.network.PacketUpdateClientTarget;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EntityTracker;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemElytra;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nullable;
 import java.util.Iterator;
 
 public class ItemHangGlider extends Item {
@@ -24,6 +30,13 @@ public class ItemHangGlider extends Item {
         super();
         setCreativeTab(OpenGlider.creativeTab);
         setUnlocalizedName(ModInfo.MODID +":" + ModInfo.ITEM_GLIDER_NAME);
+        this.addPropertyOverride(new ResourceLocation("deployed"), new IItemPropertyGetter() {
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entityIn) {
+                return entityIn != null && entityIn instanceof EntityPlayer && OpenGliderCapabilities.getIsGliderDeployed((EntityPlayer)entityIn) ? 1.0F : 0.0F;
+            }
+        });
+
     }
 
     @Override
