40164977b7c6ddc70ad34605419f02eb09879220
Max Wason <xwason@gmail.com>
2016-12-28 12:49:40 -0700

    Fix rendering in 1st person finally

--- src/main/java/gr8pefish/openglider/client/event/ClientEventHandler.java
+++ src/main/java/gr8pefish/openglider/client/event/ClientEventHandler.java
@@ -9,8 +9,6 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -69,34 +67,6 @@ public class ClientEventHandler extends Gui {
         }
     }
 
-//    @SubscribeEvent
-//    public void onRenderOverlay(RenderGameOverlayEvent.Pre event){
-//        if (Minecraft.getMinecraft().gameSettings.thirdPersonView == 0) {//first person perspective
-//            if (event.getType() == RenderGameOverlayEvent.ElementType.POTION_ICONS) {
-//                EntityPlayer playerEntity = Minecraft.getMinecraft().thePlayer;
-//                if (OpenGliderCapabilities.getIsGliderDeployed(playerEntity)) {
-//                    if (OpenGliderPlayerHelper.shouldBeGliding(playerEntity)) {
-//                        renderTriangle(event);
-//                    }
-//                }
-//            }
-//        }
-//    }
-//
-//    private void renderTriangle(RenderGameOverlayEvent event){
-//        int centeredScreenStartWidth = event.getResolution().getScaledWidth() / 2 - 128; //-x | x=2nd to last param of draw rectangle method/2
-//        int screenStartHeight = 0; //top of screen
-//        ResourceLocation rl = new ResourceLocation("openglider", "textures/hang_glider_overlay.png"); //seems to accept the texture, but wrong size?
-//        Minecraft.getMinecraft().getTextureManager().bindTexture(rl); //not working?
-//        GlStateManager.pushMatrix();
-//        GlStateManager.enableAlpha();
-//        GlStateManager.enableTexture2D();
-//        GlStateManager.rotate(75, 1, 0, 0);
-//        this.drawTexturedModalRect(centeredScreenStartWidth, screenStartHeight, 0, 90, 256, 166); //testing
-//        GlStateManager.popMatrix();
-//
-//    }
-
     /**
      * For rendering as a perspective projection in-world, as opposed to the slightly odd looking orthogonal projection above
      */
@@ -117,17 +87,20 @@ public class ClientEventHandler extends Gui {
     private void renderTriangle(RenderWorldLastEvent event){
 
         EntityPlayer entityPlayer = Minecraft.getMinecraft().thePlayer;
+        Minecraft.getMinecraft().getTextureManager().bindTexture(ModInfo.MODEL_GLIDER_TEXTURE_RL); //bind texture
 
         GlStateManager.pushMatrix(); //push matrix
-        Minecraft.getMinecraft().getTextureManager().bindTexture(ModInfo.MODEL_GLIDER_TEXTURE_RL); //bind texture
-        //not rotating, where does the 3rd person one rotate?
-//        modelGlider.setRotationAngles(entityPlayer.limbSwing, entityPlayer.limbSwingAmount, entityPlayer.getAge(), entityPlayer.rotationYawHead, entityPlayer.rotationPitch, 1, entityPlayer); //update rotation (Not working)
+        setRotationWorld(entityPlayer, event.getPartialTicks()); //set the roation correctly for 1st person
         modelGlider.render(entityPlayer, entityPlayer.limbSwing, entityPlayer.limbSwingAmount, entityPlayer.getAge(), entityPlayer.rotationYawHead, entityPlayer.rotationPitch, 1); //render
         GlStateManager.popMatrix(); //pop matrix
 
-        //doesn't rotate correctly
-        rotateToHorizontal(entityPlayer, entityPlayer.posX, entityPlayer.posY, entityPlayer.posZ);
-        GlStateManager.popMatrix();
+    }
+
+    private void setRotationWorld(EntityPlayer player, float partialTicks) {
+        double interpolatedYaw = (player.prevRotationYaw + (player.rotationYaw - player.prevRotationYaw) * partialTicks);
+        GlStateManager.rotate((float) -interpolatedYaw, 0, 1, 0); //same direction as player (but backwards)
+        GlStateManager.rotate(180F, 0, 1, 0); //rotate so it is forwards
+        GlStateManager.translate(0, 2, 0); //move up above head
     }
 
 
