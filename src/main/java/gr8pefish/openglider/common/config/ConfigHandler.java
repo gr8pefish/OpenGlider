45ec345fa6b41c4020981b55b9fe9f3741628f0f
Max Wason <xwason@gmail.com>
2016-12-31 12:17:58 -0700

    More default value tweaks

--- src/main/java/gr8pefish/openglider/common/config/ConfigHandler.java
+++ src/main/java/gr8pefish/openglider/common/config/ConfigHandler.java
@@ -55,15 +55,15 @@ public class ConfigHandler {
         category = "1) Balance";
         categories.add(category);
         forwardMovement = config.getFloat("1) Normal Forward Movement", category, 0.02F, 0, 100,"The amount of blocks to move forwards (per-tick) while gliding normally.");
-        verticalMovement = config.getFloat("2) Normal Fall Distance", category, 0.5F, 0, 100,"The amount of blocks a player falls (per-tick) while gliding normally.");
-        forwardMovementShift = config.getFloat("3) Fast Forward Movement", category, 0.09F, 0, 100,"The amount of blocks to move forwards (per-tick) while gliding fast (pressing 'Shift').");
-        verticalMovementShift = config.getFloat("4) Fast Fall Distance", category, 0.7F, 0, 100,"The amount of blocks to fall (per-tick) while gliding fast (pressing 'Shift').");
+        verticalMovement = config.getFloat("2) Normal Fall Distance", category, 0.55F, 0, 100,"The amount of blocks a player falls (per-tick) while gliding normally.");
+        forwardMovementShift = config.getFloat("3) Fast Forward Movement", category, 0.08F, 0, 100,"The amount of blocks to move forwards (per-tick) while gliding fast (pressing 'Shift').");
+        verticalMovementShift = config.getFloat("4) Fast Fall Distance", category, 0.8F, 0, 100,"The amount of blocks to fall (per-tick) while gliding fast (pressing 'Shift').");
 
         category = "2) Wind";
         categories.add(category);
         windEnabled = config.getBoolean("1) Enable Wind", category, true, "Enables wind, making the player move unpredictably around when gliding.");
-        windOverallPower = config.getFloat("2) Overall Power", category, 1.3F, 0.001F, 10, "A quality-of-life option to quickly change the overall power of the wind effect. Default is an overall relatively weak wind, with moderate gusts that occur semi-commonly. Note that this value can be a decimal (i.e. 0.5 would be half as strong). More fine-grained options are available below.");
-        windGustSize = config.getFloat("3) Gust Size", category, 20, 1, 100, "The size of the wind gusts, larger values mean the gusts push the player around in greater angles from their intended direction. Default is moderately sized. Observable gameplay effects are highly tied with wind frequency.");
+        windOverallPower = config.getFloat("2) Overall Power", category, 1.4F, 0.001F, 10, "A quality-of-life option to quickly change the overall power of the wind effect. Default is an overall relatively weak wind, with moderate gusts that occur semi-commonly. Note that this value can be a decimal (i.e. 0.5 would be half as strong). More fine-grained options are available below.");
+        windGustSize = config.getFloat("3) Gust Size", category, 19, 1, 100, "The size of the wind gusts, larger values mean the gusts push the player around in greater angles from their intended direction. Default is moderately sized. Observable gameplay effects are highly tied with wind frequency.");
         windFrequency = config.getFloat("4) Wind Frequency", category, 0.15F, 0, 5, "The frequency of the wind gusts, larger values mean the wind effects occur more often. 0 removes wind. Default is semi-common. Observable gameplay effects are highly tied with gust size.");
         windRainingMultiplier = config.getFloat("5) Rain Multiplier", category, 5, 1, 10, "How much stronger the wind should be while it is raining. 1 means the wind is the same if raining or not, 10 means the wind is 10x stronger while it is raining.");
         windSpeedMultiplier = config.getFloat("6) Speed Multiplier", category, 0.4F, -10, 10, "When going fast, the overall wind effect is changed by this multiplier. Default is that going fast reduces the wind effect by a moderate amount. 0 means the player's speed has no effect on the wind.");
