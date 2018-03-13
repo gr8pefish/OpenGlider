386fac581b07bb036709a9bcc4129ecf16c67ca2
gr8pefish <gr8pefish@gmail.com>
2018-03-13 11:16:08 -0700

    Update docs for WindHelper

--- src/main/java/gr8pefish/openglider/common/wind/WindHelper.java
+++ src/main/java/gr8pefish/openglider/common/wind/WindHelper.java
@@ -6,14 +6,27 @@ import gr8pefish.openglider.common.wind.generator.OpenSimplexNoise;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
+
+/**
+ * Applies "wind" effects to the player, moving them around when they fly.
+ */
 public class WindHelper {
 
+    /** Simplex noise has a better "game feel" than Perlin (the standard for Minecraft), so ti is used here. */
     public static OpenSimplexNoise noiseGenerator;
 
+    /** Initialize noise generator once and store in a static variable for performance. */
     public static void initNoiseGenerator() {
         noiseGenerator = new OpenSimplexNoise();
     }
 
+    /**
+     * Apply wind effect, buffeting them around pseudo-randomly.
+     * Affected by a variety of things, from player height to glider durability to weather, etc.
+     *
+     * @param player - the player to move around
+     * @param glider - the hang glider item
+     */
     public static void applyWind(EntityPlayer player, ItemStack glider){
 
         if (!ConfigHandler.windEnabled) return; //if no wind, then do nothing
@@ -33,7 +46,7 @@ public class WindHelper {
 
         //stabilize somewhat depending on velocity
         double velocity = Math.sqrt(Math.pow(player.motionX, 2) + Math.pow(player.motionZ, 2)); //player's velocity
-        double speedStabilized = noise * 1/((velocity * windSpeedMultiplier) + 1); //stabilize somewhat with higher speeds //ToDo: test
+        double speedStabilized = noise * 1/((velocity * windSpeedMultiplier) + 1); //stabilize somewhat with higher speeds
 
         //increase wind depending on world height
         double height = player.posY < 256 ? (player.posY / 256) * windHeightMultiplier : windHeightMultiplier; //world height clamp
